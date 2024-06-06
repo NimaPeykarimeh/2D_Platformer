23daf7404f5863463058451b63e8739b840c0a5f
NimaPeykarimeh <nimapeykerimeh87@gmail.com>
2024-06-07 00:34:31 +0300

    removed extras

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -7,11 +7,11 @@ public class CameraFollow : MonoBehaviour
 
     [SerializeField] Transform objectToFollow;
     [SerializeField] Vector3 offset = new Vector3(0f,0f,-10f);
-
+    [SerializeField] Vector3 speedMultiplier;
     [Range(0f,1f)]
     [SerializeField] float speed = 0.125f;
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
         transform.position = Vector3.Lerp(transform.position ,objectToFollow.position + offset,speed);
     }
