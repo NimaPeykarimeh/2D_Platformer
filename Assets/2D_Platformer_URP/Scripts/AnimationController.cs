cae1e989c4af451ebccbef7959c82f342a1e6f78
NimaPeykarimeh <nimapeykerimeh87@gmail.com>
2024-06-10 14:39:41 +0300

    Removed extras

---
+++ Assets/2D_Platformer_URP/Scripts/AnimationController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    Movement2D movement;
+    [SerializeField] Animator animator;
+    private void Awake()
+    {
+        movement = GetComponent<Movement2D>();
+    }
+    void Update()
+    {
+        animator.SetBool("Dashing", movement.isDashing);
+        animator.SetFloat("JumpSpeed", movement.currentVerticalSpeed);
+        animator.SetFloat("MovementSpeed", Mathf.Abs(movement.currentHorizontalSpeed));
+        animator.SetBool("Grounded",movement.isGrounded);
+        animator.SetBool("OnWallHit",movement.hitWall && movement.WallJump);
+        animator.SetBool("OnLedge",movement.isLedge);
+        //animator.SetBool("ClimbLedge", movement.isClimbingLedge);
+        
+    }
+}
